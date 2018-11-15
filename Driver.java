7deab75a54a9e70941538468a867facf003515ae
stefansor <ssorobay00@stuy.edu>
2018-11-15 16:23:48 -0500

    my illustrious driver hardy har

--- Driver.java
+++ Driver.java
@@ -2,6 +2,8 @@ public class Driver{
 
   public static void main(String[] args) {
     WordSearch one = new WordSearch(20, 20);
+    one.addWord("hello", 10, 10, -1, -1);
+    one.addWord("soooo",12 ,12, 1, 1);
     System.out.println(one.toString());
   }
 }
