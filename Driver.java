b720e28503847393ce60e0647851accec4cc3bad
stefansor <ssorobay00@stuy.edu>
2018-11-16 00:51:30 -0500

    hmmm trusty driver

--- Driver.java
+++ Driver.java
@@ -1,9 +1,8 @@
+import java.util.Random;
 public class Driver{
 
   public static void main(String[] args) {
     WordSearch one = new WordSearch(20, 20);
-    one.addWord("hello", 10, 10, -1, -1);
-    one.addWord("soooo",12 ,12, 1, 1);
     System.out.println(one.toString());
   }
 }
