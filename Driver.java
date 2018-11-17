2bab3165c0e7a49bc4c774306b6fc27572c05ef9
stefansor <ssorobay00@stuy.edu>
2018-11-17 13:40:41 -0500

    illutrious driver

--- Driver.java
+++ Driver.java
@@ -4,5 +4,6 @@ public class Driver{
   public static void main(String[] args) {
     WordSearch one = new WordSearch(20, 20);
     System.out.println(one.toString());
+    System.out.println(""+(int)(Math.random() * 1000));
   }
 }
